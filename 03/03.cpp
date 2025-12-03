e251dec588de2ff86db4839c8db06e05556bf6a5
Ivan Podogov <ginkage.uk@gmail.com>
2025-12-03 08:11:40 +0000

    Three

---
+++ 03/03.cpp
@@ -0,0 +1,41 @@
+#include "../lib.hpp"
+
+long memo[100][13], pw[13];
+
+long dp(const char *s, int n, int pos, int dig) {
+    long m = memo[pos][dig];
+    if (m < 0) {
+        for (int i = pos; i < n - dig + 1; i++)
+            m = max(m, pw[dig] * (s[i] - '0') + dp(s, n, i + 1, dig - 1));
+        memo[pos][dig] = m;
+    }
+    return m;
+}
+
+int main() {
+    long result = 0, result2 = 0;
+
+    pw[1] = 1;
+    for (int i = 2; i < 13; i++)
+        pw[i] = 10 * pw[i - 1];
+
+    while (true) {
+        string s;
+        getline(cin, s);
+        if (!cin) break;
+
+        for (int i = 0; i < 100; i++) {
+            memo[i][0] = 0;
+            for (int j = 1; j < 13; j++)
+                memo[i][j] = -1;
+        }
+
+        result += dp(s.c_str(), s.size(), 0, 2);
+        result2 += dp(s.c_str(), s.size(), 0, 12);
+    }
+
+    cout << result << ' ' << result2 << endl;
+
+    return 0;
+}
+
