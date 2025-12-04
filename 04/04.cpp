0696dcd93d9bdcec2d5a76db364345060641691f
Ivan Podogov <ginkage.uk@gmail.com>
2025-12-04 10:15:32 +0000

    Four

---
+++ 04/04.cpp
@@ -0,0 +1,53 @@
+#include "../lib.hpp"
+
+static int di[] = { -1, -1, -1, 0, 0, 1, 1, 1 };
+static int dj[] = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+vector<string> grid;
+vector<vector<int8_t>> cnt;
+int m, n;
+
+char get(int i, int j) { return (i < 0 || j < 0 || i >= m || j >= n) ? '.' : grid[i][j]; }
+
+int main() {
+    while (true) {
+        string s;
+        getline(cin, s);
+        if (!cin) break;
+        grid.push_back(s);
+    }
+
+    m = grid.size();
+    n = grid[0].size();
+    cnt.resize(m, vector<int8_t>(n, 0));
+
+    queue<vec> q;
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < 8; k++)
+                cnt[i][j] += get(i + di[k], j + dj[k]) == '@';
+
+            if (grid[i][j] == '@' && cnt[i][j] < 4)
+                q.emplace(i, j);
+        }
+
+    long result = q.size(), result2 = 0;
+    while (!q.empty()) {
+        auto t = q.front();
+        q.pop();
+        int i = t[0], j = t[1];
+        grid[i][j] = '.';
+        result2++;
+
+        for (int k = 0; k < 8; k++) {
+            int ii = i + di[k], jj = j + dj[k];
+            if (get(ii, jj) == '@' && --cnt[ii][jj] == 3)
+                q.emplace(ii, jj);
+        }
+    }
+
+    cout << result << ' ' << result2 << endl;
+
+    return 0;
+}
+
